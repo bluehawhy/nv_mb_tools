f3646c11e309841132e8832203d947e3669fa081
xna139j <miskang@navis-ams.com>
2023-04-05 09:55:10 +0900

    add version

--- nv_mb_tools.py
+++ nv_mb_tools.py
@@ -1,6 +1,6 @@
-from doctest import master
 import os
 import sys
+import traceback
 from PyQt5.QtWidgets import QApplication
 
 from _src._api import logger, logging_message, config, license_key, license_login
@@ -17,7 +17,8 @@ revision_list=[
     'v0.1 (2022-01-24) : proto type release (beta ver.)',
     'v0.2 (2022-01-27) : merge download trigger and extract sreenshot (beta ver.)\ndisable function button when running.',
     'v0.3 (2022-02-14) : add change binary and open firewall, port',
-    'v0.4 (2022-02-23) : Change from ssh (built-in) to putty'
+    'v0.4 (2022-02-23) : Change from ssh (built-in) to putty',
+    'v0.5 (2022-04-05) : add trace when critical error'
     ]
 
 config_path ='static\config\config.json'
@@ -32,39 +33,28 @@ def function_app():
     app = QApplication(sys.argv)
     ex = mb_tools_ui.MyMainWindow(version)
     sys.exit(app.exec_())
-    return 0
 
 def login_app():
     app = QApplication(sys.argv)
     form = license_login.MyMainWindow()
     sys.exit(app.exec_())
-    return 0
 
 def prod_app():
     license = license_key.check_License()
     lic_validation = license_key.valild_License(license)
     os.system('color 0A') if license['user'] != 'miskang' else None
-    os.system('mode con cols=70 lines=5') if license['user'] != 'miskang' else None
+    #os.system('mode con cols=70 lines=5') if license['user'] != 'miskang' else None
     if lic_validation == True:
         logging.info('license is valild - %s' %str(license))
         function_app()
     else:
         logging.info('license is invalild - %s need up to date' %str(license))
         login_app()
-            
-
 
 def debug_app():
-    user ='root'
-    ip = '10.120.1.91'
-    path_pc = r'G:\SW\E048.1_230711' 
-    a = mb_tools.download_file(user,ip,'/var/system-version.txt')
+    mb_tools.send_by_plink('root','10.120.1.91','ls')
     return 0
     
 if __name__ =='__main__':
-    try:
-        debug_app()
-    except Exception as E:
-        logging.info(E)
-        logging_message.input_message(path = message_path,message = str(E))
+    prod_app()
 
